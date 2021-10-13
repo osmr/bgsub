bf14e8d91bc259da61ce4674601600be25da8e06
Oleg Semery <osemery@gmail.com>
2021-10-13 18:18:22 +0300

    Refactoring, 2

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ pip install -r requirements.txt
 ```
 
 ## Usage
-Usual script run:
+1. Launch a script for background subtraction via benzin.io/remove.bg service:
 ```
-python subtract_bg.py --service=<service> --token=<your token> --input=<directory with images> --output=<output directory with binary masks>
+python subtract_bg_service.py --service=<service> --token=<your token> --input=<directory with images> --output=<output directory with binary masks>
 ```
 Here:
 - `service` is `benzinio` for `benzin.io` service or `removebg` for `remove.bg`,
@@ -32,6 +32,18 @@ Optional parameters:
 - `url` is an optional custom URL for service,
 - `jpg` is a flag for forced recompression an input image as JPG (should use as `--jpg`),
 - `not-resize` is a flag for suppressing forcible scale the mask to the input image (should use as `--not-resize`).
+
+2. Launch a script for background subtraction via human segmentation network:
+```
+python subtract_bg_human.py --input=<directory with images> --output=<output directory with binary masks>
+```
+Here:
+- `input` is a directory with processing JPEG images (can contain subdirectories),
+- `output` is a directory with resulted PNG binary masks (it is assumed that all original images had unique names).
+
+Optional parameters:
+- `ppdir` is a flag for adding extra parrent+parrent directory to the output one (should use as `--ppdir`).
+- `use-cuda` is a flag for using CUDA for network inference (should use as `--use-cuda`).
 
 ## Remark
 
--- subtract_bg_human.py
+++ subtract_bg_human.py
@@ -4,17 +4,11 @@
 
 import os
 import argparse
-import numpy as np
 from tqdm import tqdm
 import cv2
-from segmentation_models_pytorch import Unet
-from torch.utils import model_zoo
-import segmentation_refinement as refine
-import torch
 from utils.path import get_image_file_paths_with_subdirs, create_output_file_path, create_dubug_file_path
-from utils.image import resize_image, resize_image_with_max_size, pad_image_to_size, pad_image_by_factor, crop_image,\
-    normalize_image, create_image_with_mask
-from utils.pytorch import rename_layer_names_in_state_dict, create_tensor_from_rgb_image
+from utils.image import create_image_with_mask
+from image_human_segmenter import ImageHumanSegmenter
 
 
 def parse_args() -> argparse.Namespace:
@@ -33,27 +27,10 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--output", type=str, required=True, help="output masks directory path")
     parser.add_argument("--ppdir", action="store_true", default=False,
                         help="add extra parrent+parrent directory to the output one")
+    parser.add_argument("--use-cuda", action="store_true", help="use CUDA")
     parser.add_argument("--debug", action="store_true", default=False, help="debug mode")
     args = parser.parse_args()
     return args
-
-
-def create_human_segm_net():
-    """
-    Create human segmentation Pytorch network.
-
-    Returns:
-    -------
-    net : nn.Module
-        Target net with loaded weights.
-    """
-    model_weights_url = "https://github.com/ternaus/people_segmentation/releases/download/0.0.1/2020-09-23a.zip"
-    net = Unet(encoder_name="timm-efficientnet-b3", classes=1, encoder_weights=None)
-    state_dict = model_zoo.load_url(url=model_weights_url, progress=True, map_location="cpu")["state_dict"]
-    state_dict = rename_layer_names_in_state_dict(state_dict=state_dict, layer_name_dict={"model.": ""})
-    net.load_state_dict(state_dict)
-    net.eval()
-    return net
 
 
 if __name__ == "__main__":
@@ -62,6 +39,7 @@ if __name__ == "__main__":
     output_mask_dir_path = args.output
     add_ppdir = args.ppdir
     debug_mode = args.debug
+    use_cuda = args.use_cuda
 
     input_image_dir_path = os.path.expanduser(input_image_dir_path)
     if not os.path.exists(input_image_dir_path):
@@ -71,9 +49,7 @@ if __name__ == "__main__":
     if not os.path.exists(output_mask_dir_path):
         os.mkdir(output_mask_dir_path)
 
-    net = create_human_segm_net()
-    # refiner = refine.Refiner(device="cpu")
-    refiner = refine.Refiner(device='cuda:0')
+    segmenter = ImageHumanSegmenter(use_cuda)
 
     image_file_path_list = get_image_file_paths_with_subdirs(input_image_dir_path)
 
@@ -87,40 +63,7 @@ if __name__ == "__main__":
             continue
 
         bgr_image = cv2.imread(image_file_path, flags=cv2.IMREAD_UNCHANGED)
-        rgb_image = cv2.cvtColor(bgr_image, code=cv2.COLOR_BGR2RGB)
-
-        base_size = 800
-        rgb_image, rgb_image_size = resize_image_with_max_size(rgb_image, max_size=base_size)
-        rgb_image, rgb_image_pad_params = pad_image_to_size(
-            image=rgb_image,
-            dst_image_size=(base_size, base_size),
-            border_type=cv2.BORDER_CONSTANT)
-
-        padded_image, factor_pad_params = pad_image_by_factor(
-            image=rgb_image,
-            factor=32,
-            border_type=cv2.BORDER_CONSTANT)
-        x = normalize_image(image=padded_image)
-        x = create_tensor_from_rgb_image(x)
-        x = torch.unsqueeze(x, dim=0)
-
-        with torch.no_grad():
-            y = net(x)[0][0]
-
-        mask = (y > 0).cpu().detach().numpy().astype(np.uint8)
-        mask = mask * 255
-
-        mask = refiner.refine(rgb_image, mask, fast=True, L=1200)
-
-        mask = crop_image(mask, factor_pad_params)
-
-        mask = crop_image(mask, rgb_image_pad_params)
-        mask = resize_image(mask, image_size=rgb_image_size)
-
-        mask = refiner.refine(bgr_image, mask, fast=True, L=1800)
-        mask = (mask > 127).astype(np.uint8) * 255
-        assert (mask.dtype == np.uint8)
-
+        mask = segmenter(bgr_image)
         cv2.imwrite(mask_file_path, mask)
 
         if debug_mode:
